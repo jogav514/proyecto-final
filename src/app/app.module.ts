be98f4893ec3c69f4f4bda9cfae358041fc4a7aa
jogav514 <esteban1gaviria@gmail.com>
2023-02-02 01:01:56 -0500

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { UsComponent } from './us/us.component';
+import { ProgramsComponent } from './programs/programs.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    UsComponent,
+    ProgramsComponent,
+    ContactComponent,
+    ProfileComponent,
+    LoginComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
