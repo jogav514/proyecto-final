b25ec78717a157c3e1d005159688a6dd876642c5
jogav514 <esteban1gaviria@gmail.com>
2023-02-02 13:38:40 -0500

    footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -31,7 +31,12 @@ const routes: Routes = [
   {
     path:'login',
     component:LoginComponent,
-  }
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
 ];
 
 @NgModule({
